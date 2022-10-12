009cfdc4c4a133d492ed7e1f2adf7b802dd90520
Tomasjaggard <81049049+Tomasjaggard@users.noreply.github.com>
2022-10-12 02:04:50 +0100

    Updated main.py
    -Added ability for users to replace their wallet address in the csv with a new one
    -Added a "flag" system to detect if a user already has a wallet in the csv
    -Changed responses to ephemeral to reduce spam in servers
    -Cleaned up the code overall & removed redundancies

--- main.py
+++ main.py
@@ -6,38 +6,46 @@ from webserver import keep_alive
 import csv
 
 class aclient(discord.Client):
-    def __init__(self):
-        super().__init__(intents=discord.Intents.default())
-        self.synced = False
 
-    async def on_ready(self):
-        await self.wait_until_ready()
-        if not self.synced:
-            await tree.sync(guild = discord.Object(id = 924742107661492254))
-            self.synced = True
-        print(f"We have logged in as {self.user}.")
+  def __init__(self):
+    super().__init__(intents=discord.Intents.default())
+    self.synced = False
+
+  async def on_ready(self):
+    await self.wait_until_ready()
+    if not self.synced:
+      await tree.sync(guild=discord.Object(id=924742107661492254))
+      self.synced = True
+    print(f"We have logged in as {self.user}.")
 
 client = aclient()
 tree = app_commands.CommandTree(client)
 
-@tree.command(name = "whitelist", description = "Add your 0x to the whitelist", guild = discord.Object(id = 924742107661492254))
-async def self(interaction: discord.Interaction,number: str):
-    name = interaction.user.id
-    input = [name,number]
-    duplicate = False
-    with open ('data.csv', 'r+') as file:
-      
-      reader = csv.reader(file, delimiter=',')
-      for row in reader:
-        print(name, row[0])
-        if str(name) == row[0]:
-          await interaction.response.send_message("Your wallet is already set")
-          return None
-      writer = csv.writer(file)
-      writer.writerow(input)
-      print("New entry added")
-      await interaction.response.send_message("Your wallet has been added!")
+@tree.command(name="whitelist", description="Add your 0x to the whitelist", guild=discord.Object(id=924742107661492254))
+async def self(interaction: discord.Interaction, number: str):
+  name = interaction.user.id
+  details = [name, number]
+  flag = False
+  contents = []
+  with open('data.csv', 'r') as file:
+    reader = csv.reader(file, delimiter=',')
+    contents.extend(reader)
 
+  with open('data.csv', 'w') as file:
+    writer = csv.writer(file)
+    for row in contents:
+      print(row)
+      if str(name) == row[0]:
+        row[1] = number
+        flag = True
+        await interaction.response.send_message(
+          "Your new wallet address has been set. Old wallet deleted.",
+          ephemeral=True)
+      writer.writerow(row)
+    if flag == False:
+      writer.writerow(details)
+      await interaction.response.send_message("Your wallet has been added!", ephemeral=True)
+      print("New entry added")
 
 keep_alive()
 
